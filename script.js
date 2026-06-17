e82e900f32afea281f16d19a829b1fb986e66cc7
alfredolimsur-sys <alfredolimsur@gmail.com>
2026-06-16 20:25:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-fetch("data/perfil.json")
-.then(res => res.json())
-.then(data => {
-    document.querySelector(".hero h2").innerText = data.nombre;
-    document.querySelector(".hero h3").innerText = data.rol;
-    document.querySelector(".hero p").innerText = data.descripcion;
-});
-
 fetch("data/proyectos.json")
 .then(res => res.json())
 .then(data => {
-    const cont = document.querySelector("#experiencia .grid");
+    const cont = document.getElementById("exp");
 
     data.forEach(p => {
         cont.innerHTML += `
@@ -18,8 +10,9 @@ fetch("data/proyectos.json")
             <h3>${p.titulo}</h3>
             <p>${p.descripcion}</p>
         </div>`;
+    });
 });
- });
+
 fetch("data/negocio.json")
 .then(res => res.json())
 .then(data => {
@@ -28,12 +21,10 @@ fetch("data/negocio.json")
     data.forEach(p => {
         cont.innerHTML += `
         <div class="card">
-            <img src="${p.imagen}" style="width:100%; border-radius:10px; margin-bottom:10px;">
+            <img src="${p.imagen}" class="card-img">
             <h3>${p.nombre}</h3>
             <p>${p.descripcion}</p>
-            <strong style="color:#25D366;">${p.precio}</strong>
+            <strong>${p.precio || ""}</strong>
         </div>`;
-    });
-});
     });
 });
