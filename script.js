747f31c60aa100f5a23183583a26901db6a3e1b8
alfredolimsur-sys <alfredolimsur@gmail.com>
2026-06-16 20:11:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,17 +19,21 @@ fetch("data/proyectos.json")
             <p>${p.descripcion}</p>
         </div>`;
 });
-
+ });
 fetch("data/negocio.json")
 .then(res => res.json())
 .then(data => {
-    const cont = document.querySelector("#negocio .grid");
+    const cont = document.getElementById("shop");
 
     data.forEach(p => {
         cont.innerHTML += `
         <div class="card">
-            <h3>${p.titulo}</h3>
+            <img src="${p.imagen}" style="width:100%; border-radius:10px; margin-bottom:10px;">
+            <h3>${p.nombre}</h3>
             <p>${p.descripcion}</p>
+            <strong style="color:#25D366;">${p.precio}</strong>
         </div>`;
+    });
+});
     });
 });
